46be04cc5e1df5209b0397c49b5eb7bbf6206e17
Mustafa Mohamed Ahmed Tag Eldeen <51303436+TAGOOZ@users.noreply.github.com>
2025-07-19 20:05:47 +0300

    Updated pendingRequests.ts

--- src/services/pendingRequests.ts
+++ src/services/pendingRequests.ts
@@ -278,7 +278,7 @@ import { supabase } from '../lib/supabase';
             .from('family_members')
             .insert([{
               family_id: familyId,
-              created_by: fetchedRequest.submitted_by
+              created_by: fetchedRequest.submitted_by,
               role: 'parent'
             }]);
             
